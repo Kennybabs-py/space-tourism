79edb886f811f13d6a00b6fc32a08f296460fe34
Kennybabs-py <babalolakehinde74@gmail.com>
2022-10-19 14:19:25 +0100

    fonts added locally; updates in nav

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import style from "./styles.module.scss";
 
+const listTab = ["destination", "crew", "technology"];
+
 const Header = () => {
-  const [value, setValue] = useState<number>();
   const [toggle, setToggle] = useState(false);
 
-  const handleToggle = (): void => {
-    setToggle((prev) => !prev);
-  };
+  const router = useRouter();
 
-  const listTab: string[] = ["Home", "destination", "crew", "technology"];
+  function handleToggle() {
+    setToggle((prev) => !prev);
+  }
+
+  function handleRouting(path: string) {
+    router.push(path);
+    setToggle(false);
+  }
 
   return (
     <nav className={style.nav}>
@@ -20,39 +27,62 @@ const Header = () => {
 
       <hr className={style.line} />
 
-      <div className={style.ham_box} onClick={handleToggle}>
-        <div
-          className={`${style.hamburger} ${toggle ? style.active : ""}`}
-        ></div>
-      </div>
+      {/* desktop */}
+      <div className={style.desktop_nav}>
+        <Link href="/">
+          <a
+            className={`${router.asPath === "/" ? style.active_tab : null}`}
+            onClick={() => handleRouting("/")}
+          >
+            00 home
+          </a>
+        </Link>
 
-      {/* mobile and hamburger */}
-      <div className={`${style.hamburger_menu} ${toggle ? style.active : ""}`}>
         {listTab.map((item, index) => {
           return (
-            <Link href={item === "Home" ? "/" : `/${item}`} key={index}>
+            <Link href={`/${item}`} key={index}>
               <a
-                key={index}
-                onClick={() => setValue(index)}
-                className={`'' ${index === value && "active-tab"}`}
+                onClick={() => handleRouting(`/${item}`)}
+                className={`${
+                  router.asPath === "/" + item ? style.active_tab : null
+                }`}
               >
-                {item}
+                0{index + 1} {item}
               </a>
             </Link>
           );
         })}
       </div>
 
-      {/* desktop */}
-      <div className={style.tab}>
+      {/* mobile and hamburger */}
+
+      <div className={style.hamburger_container} onClick={handleToggle}>
+        <div
+          className={`${style.hamburger} ${toggle ? style.active : ""}`}
+        ></div>
+      </div>
+
+      <div className={`${style.mobile_nav} ${toggle ? style.active : ""}`}>
+        <Link href="/">
+          <a
+            className={`${router.asPath === "/" ? style.active_tab : null}`}
+            onClick={() => handleRouting("/")}
+          >
+            home
+          </a>
+        </Link>
+
         {listTab.map((item, index) => {
           return (
-            <Link href={item === "Home" ? "/" : `/${item}`} key={index}>
+            <Link href={`/${item}`} key={index}>
               <a
-                onClick={() => setValue(index)}
-                className={index === value ? style.active_tab : ""}
+                key={index}
+                onClick={() => handleRouting(`/${item}`)}
+                className={`${
+                  router.asPath === "/" + item ? style.active_tab : null
+                }`}
               >
-                0{index} {item}
+                {item}
               </a>
             </Link>
           );
